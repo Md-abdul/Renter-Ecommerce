d1d01c7e291893fecee1175e5773f5388190b185
Md Kamran <ka5452488@gmail.com>
2025-06-24 13:01:39 +0530

    new commit

--- client_side/src/context/CartContext.jsx
+++ client_side/src/context/CartContext.jsx
@@ -19,7 +19,7 @@ export const CartProvider = ({ children }) => {
   const navigate = useNavigate();
 
   // Use local backend for development
-  const API_BASE_URL = "http://localhost:5000/api";
+  const API_BASE_URL="http://localhost:5000/api";
 
   // Calculate total price of items in cart
   // const getTotalPrice = () => {
