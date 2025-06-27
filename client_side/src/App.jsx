0a691a1ac91415c0c19c0dcd06b971050932b44b
Md-abdul <mdabdulq62@gmail.com>
2025-06-27 16:30:08 +0530

    ui changes in admin side

--- client_side/src/App.jsx
+++ client_side/src/App.jsx
@@ -1,22 +1,41 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { TopNavbar } from "./Components/HomePage/Navbar/Navbar";
 import AllRoutes from "./Routes/AllRoutes";
 import Footer from "./Components/HomePage/Footer/Footer";
 
 function App() {
-  const isAdmin = useSelector((state) => state?.UsersReducer.isAdmin); // Ensure this matches your Redux state structure
-  // console.log(isAdmin);
+  const isAdmin = useSelector((state) => state?.UsersReducer.isAdmin);
+
+  useEffect(() => {
+    // Check on app load
+    const isSessionActive = sessionStorage.getItem("isSessionActive");
+
+    if (!isSessionActive) {
+      // Session marker is gone (tab was closed)
+      // But localStorage still has old tokens
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      localStorage.removeItem("adminToken");
+      localStorage.removeItem("admin");
+
+      // Reload the app
+      window.location.reload();
+    }
+
+    // Always set the session marker for this tab
+    sessionStorage.setItem("isSessionActive", "true");
+  }, []);
 
   return (
-    <React.Fragment>
+    <>
       {!isAdmin && <TopNavbar />}
       <div style={{ marginTop: "50px" }}>
         <AllRoutes />
       </div>
       {!isAdmin && <Footer />}
-    </React.Fragment>
+    </>
   );
 }
 
